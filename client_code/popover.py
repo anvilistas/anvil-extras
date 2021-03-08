5e27ff830a0d7083d53ea7c17a07f49f5786f262
mrcork <stucork@gmail.com>
2021-03-08 21:45:57 +0800

    change licence to match the rest of the repo

--- client_code/popover.py
+++ client_code/popover.py
@@ -1,23 +1,39 @@
+# MIT License
+#
+# Copyright (c) 2021 The Anvil Extras project team members listed at
+# https://github.com/anvilistas/anvil-extras/graphs/contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This software is published at https://github.com/anvilistas/anvil-extras
+#
+#
+# for more information visit the w3 bootstrap popover page
+# https://www.w3schools.com/bootstrap4/bootstrap_ref_js_popover.asp
+#
+# or the bootstrap popover page for v 3.3.7
+# https://bootstrapdocs.com/v3.3.6/docs/javascript/#popovers
+#
+
 import anvil as _anvil
 from anvil.js.window import jQuery as _S, window as _window, document as _document
-"""
-    popover
-    Copyright 2020 Stu Cork
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Source code published at https://github.com/s-cork/popover
-
-    for more information visit the w3 bootstrap popover page
-    https://www.w3schools.com/bootstrap4/bootstrap_ref_js_popover.asp
-
-    or the bootstrap popover page for v 3.3.7
-    https://bootstrapdocs.com/v3.3.6/docs/javascript/#popovers
-"""
 
 def popover(self, content,
             title='',
