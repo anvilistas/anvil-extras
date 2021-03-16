86b5df3cff9681841e09e59648f0afd6b125f518
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2021-03-16 10:37:40 +0000

    Saved version

--- client_code/PageBreak/__init__.py
+++ client_code/PageBreak/__init__.py
@@ -22,13 +22,24 @@
 # SOFTWARE.
 #
 # This software is published at https://github.com/anvilistas/anvil-extras
-import anvil.server
-
+from anvil.js.window import jQuery as _S
+import anvil
 from ._anvil_designer import PageBreakTemplate
 
 __version__ = "1.0.0"
 
 
 class PageBreak(PageBreakTemplate):
-    def __init__(self, **properties):
+    def __init__(self, margin_top, **properties):
+        self.margin_node = _S(anvil.js.get_dom_node(self)).find(".margin-element")
+        self.margin_top = margin_top
         self.init_components(**properties)
+
+    @property
+    def margin_top(self):
+        return self._margin_top
+
+    @margin_top.setter
+    def margin_top(self, value):
+        self.margin_node.css("margin-top", value)
+        self._margin_top = value
