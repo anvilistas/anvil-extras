bc477000aa68605008026ea9b74810c7875f72d5
mrcork <stucork@gmail.com>
2021-03-16 17:20:46 +0800

    add license to all files

--- client_code/routing/_routing.py
+++ client_code/routing/_routing.py
@@ -1,3 +1,31 @@
+# MIT License
+#
+# Copyright (c) 2021 The Anvil Extras project team members listed at
+# https://github.com/anvilistas/anvil-extras/graphs/contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This software is published at https://github.com/anvilistas/anvil-extras
+
+__version__ = "1.0.0"
+
+
 from collections import namedtuple as _namedtuple
 
 import anvil as _anvil
@@ -5,7 +33,6 @@ import anvil as _anvil
 from . import _navigation
 from ._logging import logger
 
-__version__ = "1.0.0"
 
 # to print route logging messages set routing.logger.debug = True above your main_router form
 logger.debug = False
