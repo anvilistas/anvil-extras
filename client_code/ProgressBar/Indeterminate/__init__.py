2af57b7ec81179aafc82de0c2f670045bb5d9f4f
Owen Campbell <owen.campbell@empiria.co.uk>
2021-05-30 19:44:22 +0100

    [#54] Replace get_uid with get_dom_node_id

--- client_code/ProgressBar/Indeterminate/__init__.py
+++ client_code/ProgressBar/Indeterminate/__init__.py
@@ -21,17 +21,14 @@ class Indeterminate(IndeterminateTemplate):
         dom_node = get_dom_node(self)
         dom_node.style.setProperty("background-color", indicator_colour)
 
-        self.uid = session.get_uid()
+        indicator_id = session.get_dom_node_id(self.indicator_panel)
         css = f"""
-.anvil-role-indeterminate-progress-indicator-{self.uid}:before {{
+#{indicator_id}:before {{
   background-color: {track_colour}
 }}
 """
         session.style_injector.inject(css)
         self.role = "progress-track"
-        self.indicator_panel.role = [
-            "indeterminate-progress-indicator",
-            f"indeterminate-progress-indicator-{self.uid}",
-        ]
+        self.indicator_panel.role = "indeterminate-progress-indicator"
         self.indicator_panel.background = indicator_colour
         self.init_components(**properties)
--- client_code/session.py
+++ client_code/session.py
@@ -6,13 +6,18 @@
 # This software is published at https://github.com/anvilistas/anvil-extras
 import random
 
+from anvil.js import get_dom_node
+
 from . import style
 
 __version__ = "1.2.0"
+characters = "abcdefghijklmnopqrstuvwxyz0123456789"
 
 style_injector = style.Injector()
 
 
-def get_uid(length=8):
-    characters = "abcdefghijklmnopqrstuvwxyz0123456789"
-    return "".join([random.choice(characters) for _ in range(length)])
+def get_dom_node_id(component):
+    node = get_dom_node(component)
+    if not node.id:
+        node.id = "".join([random.choice(characters) for _ in range(8)])
+    return node.id
