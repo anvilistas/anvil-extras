8e88f87f9f7c46e4dcc9942bd39aa062a154f5c3
juanpaul101 <32019015+juanpaul101@users.noreply.github.com>
2021-07-08 16:20:42 +0200

    Update __init__.py

--- client_code/Autocomplete/__init__.py
+++ client_code/Autocomplete/__init__.py
@@ -34,7 +34,7 @@ _style_injector.inject(
     max-height: 300px;
     transition: all 100ms ease;
     background-color: #fff;
-    border-radius: 2px;
+    border-radius: 0px;
     z-index: 2;
     box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
 }
