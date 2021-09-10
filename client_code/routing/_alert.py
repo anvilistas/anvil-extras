61377d013a1d61a8b171e9b6c817ca91e9602e7f
Stu Cork <stu@anvil.works>
2021-09-10 10:33:09 +0800

    Improve internal code for routing.alert. We need sentinel values since the presence of a value in an anvil alert changes the behaviour.

--- client_code/routing/_alert.py
+++ client_code/routing/_alert.py
@@ -9,6 +9,7 @@ __version__ = "1.5.2"
 
 from anvil import Label
 from anvil import alert as anvil_alert
+from anvil import confirm as anvil_confirm
 
 from . import _navigation
 
@@ -17,9 +18,14 @@ class AlertInfo:
     def __init__(self, active=False, dismissible=True, content=None):
         self.update(active, dismissible, content)
 
-    def dismiss(self):
-        if self.content is not None:
+    def dismiss(self) -> bool:
+        """returns a bool as to whether the alert was dismissed"""
+        if not self.active:
+            return True  # nothing to dismiss
+        if self.dismissible and self.content is not None:
             self.content.raise_event("x-close-alert")
+            return True
+        return False
 
     def update(self, active, dismissible=True, content=None):
         self.active = active
@@ -28,40 +34,75 @@ class AlertInfo:
 
 
 current_alert = AlertInfo()
-
-alert_btns = (("OK", True, "success"),)
-confirm_btns = (("No", False, "danger"), ("Yes", True, "success"))
-# defauult buttons in anvil included here for autocomplete
-# and because providing any value overrides anvil's default buttons
+no_arg = object()
+# we use sentinel since the presence of an arg determines the behaviour of an alert
+alert_kws = ("title", "buttons", "large", "role")
 
 
+def wrap_alert(content, dismissible, is_confirm=False, **kws):
+    if isinstance(content, str):
+        content = Label(text=content)
+    current_alert.update(True, dismissible, content)
+    for key in alert_kws:
+        if kws.get(key) is no_arg:
+            del kws[key]
+            # remove sentinel values
+    try:
+        if is_confirm:
+            return anvil_confirm(content, dismissible=dismissible, **kws)
+        else:
+            return anvil_alert(content, dismissible=dismissible, **kws)
+    finally:
+        current_alert.update(False)
+
+
+# we keep the same order as defined in the api docs
+# use kws for future proofing additional anvil args
 def alert(
-    content, title="", buttons=alert_btns, large=False, dismissible=True, role=None
+    content,
+    *,
+    title=no_arg,
+    buttons=no_arg,
+    large=no_arg,
+    dismissible=True,
+    role=no_arg,
+    **kws
 ):
     """use in the same way as anvil.alert
     If dismissible=True when a user navigates the alert will be closed
     If dismissible=False navigating will be prevented
     """
-    if isinstance(content, str):
-        content = Label(text=content)
-    current_alert.update(True, dismissible, content)
-    try:
-        return anvil_alert(
-            content,
-            title=title,
-            buttons=buttons,
-            large=large,
-            dismissible=dismissible,
-            role=role,
-        )
-    finally:
-        current_alert.update(False)
+    return wrap_alert(
+        content,
+        dismissible,
+        title=title,
+        buttons=buttons,
+        large=large,
+        role=role,
+        **kws
+    )
 
 
 def confirm(
-    content, title="", buttons=confirm_btns, large=False, dismissible=False, role=None
+    content,
+    *,
+    title=no_arg,
+    buttons=no_arg,
+    large=no_arg,
+    dismissible=False,
+    role=no_arg,
+    **kws
 ):
-    return alert(content, title, buttons, large, dismissible, role)
+    return wrap_alert(
+        content,
+        dismissible,
+        is_confirm=True,
+        title=title,
+        buttons=buttons,
+        large=large,
+        role=role,
+        **kws
+    )
 
 
 def handle_alert_unload() -> bool:
@@ -69,11 +110,7 @@ def handle_alert_unload() -> bool:
     if there is an active alert which is not dismissible then navigation is prevented
     return value indicates whether this function took control of the on_navigation
     """
-    if not current_alert.active:
+    if current_alert.dismiss():
         return False
-    elif current_alert.dismissible:
-        current_alert.dismiss()
-        return False
-    else:
-        _navigation.stopUnload()
-        return True
+    _navigation.stopUnload()
+    return True
