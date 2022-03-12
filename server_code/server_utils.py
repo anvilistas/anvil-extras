c4c47323f44293dd33d9a0c100bf34009cfa6186
stu <stu@anvil.works>
2022-03-12 23:38:26 +0800

    Remove unused modules

--- server_code/server_utils.py
+++ server_code/server_utils.py
@@ -4,9 +4,6 @@
 # https://github.com/anvilistas/anvil-extras/graphs/contributors
 #
 # This software is published at https://github.com/anvilistas/anvil-extras
-from functools import wraps
-from time import time
-
 __version__ = "1.9.0"
 
 from .utils._timed import timed
