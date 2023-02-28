5f838769c5f222eed6574c664222757e0e215341
Owen Campbell <owen.campbell@empiria.co.uk>
2023-02-28 10:19:58 +0000

    Fix licence text

--- client_code/persistence.py
+++ client_code/persistence.py
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2021 anvilistas
+#
+# Copyright (c) 2021 The Anvil Extras project team members listed at
+# https://github.com/anvilistas/anvil-extras/graphs/contributors
+#
+# This software is published at https://github.com/anvilistas/anvil-extras
 import anvil.server
 
 __version__ = "2.1.4"
