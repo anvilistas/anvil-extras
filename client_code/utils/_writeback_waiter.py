53f5751c8d6970bc521d11ee1dfa95ec85cc2d0b
stu <stu@anvil.works>
2023-07-15 17:57:30 +0800

    wait_for_writeback: pure python implementation

--- client_code/utils/_writeback_waiter.py
+++ client_code/utils/_writeback_waiter.py
@@ -5,68 +5,58 @@
 #
 # This software is published at https://github.com/anvilistas/anvil-extras
 
-from functools import cache, wraps
+from functools import wraps as _wraps
+from time import sleep as _sleep
 
-from anvil.js.window import Function, anvilFormTemplates
+from anvil import Component as _Component
+from anvil.js import window as _window
 
 __version__ = "2.4.0"
 
-_store_writebacks = Function(
-    "form",
-    """
-    const self = PyDefUtils.unwrapOrRemapToPy(form);
-    const bindings = self._anvil.dataBindings || [];
-    const activeWritebacks = bindings.map(() => null);
-
-    bindings.forEach(({pyComponent}, i) => {
-        const bindingFunc = pyComponent._anvil.dataBindingWriteback;
-        const wrapper = (component, name, val) => {
-            const res = bindingFunc(component, name, val).finally(() => {
-                activeWritebacks[i] = null;
-            });
-            activeWritebacks[i] = res;
-            return res;
-        };
-        pyComponent._anvil.dataBindingWriteback = wrapper;
-    });
-
-    self._anvil.activeWritebacks = activeWritebacks;
-""",
-)
-
-_wait_for_writeback = Function(
-    "form",
-    """
-    const self = PyDefUtils.unwrapOrRemapToPy(form);
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(Promise.allSettled(self._anvil.activeWritebacks.slice(0))));
-    }).then(() => Sk.builtin.none.none$);
-""",
-)
+_active_writebacks = []
 
 
-def _init_wrapper(init):
-    @wraps(init)
-    def wrapper(self, *args, **kwargs):
-        _store_writebacks(self)
-        return init(self, *args, **kwargs)
+class _Deferred:
+    def init_promise(self, resolve, reject):
+        self.resolve = resolve
+        self.reject = reject
 
-    return wrapper
-
-
-@cache
-def _init_writeback(template):
-    template.init_components = template.__init__ = _init_wrapper(template.__init__)
+    def __init__(self):
+        self.promise = _window.Promise(self.init_promise)
 
 
-for template in anvilFormTemplates:
-    _init_writeback(template)
+class _WritebackWaiter:
+    def __init__(self):
+        self.deferred = _Deferred()
+
+    def __enter__(self):
+        _active_writebacks.append(self.deferred.promise)
+
+    def __exit__(self, exc, *args):
+        global _active_writebacks
+        promise = self.deferred.promise
+        _active_writebacks = [p for p in _active_writebacks if p is not promise]
+        self.deferred.resolve()
+
+
+_old_raise = _Component.raise_event
+
+
+def raise_event(self, event_name, **kws):
+    if not event_name.startswith("x-anvil-write-back"):
+        return _old_raise(self, event_name, **kws)
+    with _WritebackWaiter():
+        return _old_raise(self, event_name, **kws)
+
+
+_Component.raise_event = raise_event
 
 
 def wait_for_writeback(fn):
-    @wraps(fn)
-    def wrapper(self, *args, **kwargs):
-        _wait_for_writeback(self)
-        return fn(self, *args, **kwargs)
+    @_wraps(fn)
+    def wrapper(self, *args, **kws):
+        _sleep(0)
+        _window.Promise.allSettled(_active_writebacks)
+        return fn(self, *args, **kws)
 
     return wrapper
