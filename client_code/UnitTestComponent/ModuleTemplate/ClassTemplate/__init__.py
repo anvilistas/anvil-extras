5200245d5ccb9cde825025320b22ed2b9e59a0e6
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-08-01 01:06:29 +0000

    Added docstrings to test modules, classes, and methods

--- client_code/UnitTestComponent/ModuleTemplate/ClassTemplate/__init__.py
+++ client_code/UnitTestComponent/ModuleTemplate/ClassTemplate/__init__.py
@@ -11,10 +11,12 @@ class ClassTemplate(ClassTemplateTemplate):
         self.cp_module = anvil.ColumnPanel(role=self.item['card_role'])
         self.fp_runs = anvil.FlowPanel(align='left')
         self.btn_run = anvil.Button(role=self.item['btn_role'], text=self.item['name'])
+        self.lbl_doc = anvil.Label(text=self.item['ref'].__doc__)
         self.lbl_success = anvil.Label(icon='fa:check-circle', foreground='#4f7a28',
                                        visible=False, font_size=self.item['check_size'])
         
         self.fp_runs.add_component(self.btn_run)
+        self.fp_runs.add_component(self.lbl_doc)
         self.fp_runs.add_component(self.lbl_success)
         self.cp_module.add_component(self.fp_runs)
         self.add_component(self.cp_module)
---
+++ client_code/test_unittest.py
@@ -0,0 +1,16 @@
+"""This is a test module."""
+import unittest
+import anvil.server
+import time
+
+class TestClass(unittest.TestCase):
+    """This is a testclass"""
+
+    def test_method_1(self):
+        """Test Method 1."""
+        pass
+
+    def test_method_2(self):
+        """Test Method 2"""
+        pass
+        
