8460a98120fdb921c68052d7cbb98885dcd19f14
stu <stu@anvil.works>
2023-09-17 15:22:54 +0800

    Revert "Navigation: set default mode and allow titles in menu definition"
    This reverts commit e9344ae8b470c8df657ae2237dcd8605839f208f.

--- client_code/navigation.py
+++ client_code/navigation.py
@@ -22,24 +22,9 @@ _visibility = {}
 
 _title_label = Label()
 
-_default_mode = "classic"
-
-
-def _validate_mode(mode):
-    if mode not in ("classic", "hash"):
-        raise ValueError(
-            "A navigation link's routing must either be 'classic' or 'hash'"
-        )
-
-
-def set_mode(mode):
-    global _default_mode
-    _validate_mode(mode)
-    _default_mode = mode
-
 
 class register:
-    """A decorator to register a form as a navigation target (only use in classic mode)"""
+    """A decorator to register a form in the _forms dict."""
 
     def __init__(self, name, title=None):
         self.name = name
@@ -65,9 +50,7 @@ def set_title(text):
 def open_form(form_name, *args, full_width=False, **kwargs):
     """Use classic routing to open a registered form"""
     form = get_form(form_name, *args, **kwargs)
-    title = _forms[form_name]["title"]
-    if title is not None:
-        set_title(title)
+    set_title(_forms[form_name]["title"])
     get_open_form().content_panel.clear()
     get_open_form().content_panel.add_component(form, full_width_row=full_width)
 
@@ -92,9 +75,6 @@ def _reset_links():
             link.role = []
 
 
-_actions = {"classic": open_form, "hash": set_url_hash}
-
-
 def _default_link_click(**event_args):
     """A handler for navigation link click events
     * Clears the role of all links registered in this module
@@ -104,13 +84,11 @@ def _default_link_click(**event_args):
     _reset_links()
     link = event_args["sender"]
     link.role += ["selected"]
-
+    actions = {"classic": open_form, "hash": set_url_hash}
     kwargs = {}
     if link.tag.routing == "classic":
         kwargs["full_width"] = link.tag.full_width
-    if link.tag.title is not None:
-        set_title(link.tag.title)
-    _actions[link.tag.routing](link.tag.target, **kwargs)
+    actions[link.tag.routing](link.tag.target, **kwargs)
 
 
 def _visibility_event_handler(**event_args):
@@ -147,10 +125,9 @@ def build_menu(container, items, with_title=True):
 
 
 def navigation_link(
-    routing=None,
+    routing="classic",
     full_width=False,
     target=None,
-    title=None,
     on_click=None,
     visibility=None,
     **kwargs,
@@ -159,13 +136,13 @@ def navigation_link(
 
     Parameters
     ----------
+    routing
+      Either 'classic' or 'hash'
     full_width
       Whether the link target should open as full width
     target
       Either the name of a registered form for classic routing or
       a url_hash for hash routing
-    title
-      sets the page title when this link is clicked
     on_click
       event handler to call when clicked
     visibility
@@ -173,15 +150,15 @@ def navigation_link(
     kwargs
       will be passed the Link constructor
     """
-    routing = routing or _default_mode
-    _validate_mode(routing)
-
+    if routing not in ("classic", "hash"):
+        raise ValueError(
+            "A navigation link's routing must either be 'classic' or 'hash'"
+        )
     link = Link(**kwargs)
     link.tag.routing = routing
     link.tag.full_width = full_width
     link.tag.target = target
     link.tag.visibility = visibility
-    link.tag.title = title
     if on_click is None:
         link.set_event_handler("click", _default_link_click)
     else:
