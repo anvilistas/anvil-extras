520c4bc97269e444afefe56ab4e05edeeb6b5161
Owen Campbell <owen.campbell@empiria.co.uk>
2024-08-01 09:58:22 +0100

    Add licence text and version to colors.py

--- client_code/colors.py
+++ client_code/colors.py
@@ -1,4 +1,13 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021 The Anvil Extras project team members listed at
+# https://github.com/anvilistas/anvil-extras/graphs/contributors
+#
+# This software is published at https://github.com/anvilistas/anvil-extras
+
 import anvil
+
+__version__ = "2.6.2"
 
 M3_DEFAULT_SCHEMES = {
     "Material": {
