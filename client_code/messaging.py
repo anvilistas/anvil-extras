e545235b7613504eb58c0d767ef8ec2b86f59174
Duro <gabriel.duro@sonner.com.br>
2024-11-01 12:30:54 -0300

    Added support for Logger usage in messaging.py

--- client_code/messaging.py
+++ client_code/messaging.py
@@ -4,6 +4,8 @@
 # https://github.com/anvilistas/anvil-extras/graphs/contributors
 #
 # This software is published at https://github.com/anvilistas/anvil-extras
+
+from logging import Logger
 __version__ = "3.0.0"
 
 
@@ -20,46 +22,71 @@ class Subscriber:
 
 
 class Publisher:
-    def __init__(self, with_logging=True):
+    def __init__(self, with_logging=True, log_level="INFO"):
         self.with_logging = with_logging
         self.subscribers = {}
+        self.log_level = log_level
 
-    def publish(self, channel, title, content=None, with_logging=None):
+    def publish(self, channel, title, content=None, with_logging=None, log_level=None):
         if with_logging is None:
             with_logging = self.with_logging
+        if log_level is None:
+            log_level = self.log_level
         message = Message(title, content)
         subscribers = self.subscribers.get(channel, [])
         for subscriber in subscribers:
             subscriber.handler(message)
         if with_logging:
-            print(
-                f"Published '{message.title}' message on '{channel}' channel to "
-                f"{len(subscribers)} subscriber(s)"
-            )
+            if isinstance(with_logging, Logger):
+                with_logging.log(
+                    log_level,
+                    f"Published '{message.title}' message on '{channel}' channel to "
+                    f"{len(subscribers)} subscriber(s)"
+                )
+            else:
+                print(
+                    f"Published '{message.title}' message on '{channel}' channel to "
+                    f"{len(subscribers)} subscriber(s)"
+                )
 
-    def subscribe(self, channel, subscriber, handler, with_logging=None):
+    def subscribe(self, channel, subscriber, handler, with_logging=None, log_level=None):
         if with_logging is None:
             with_logging = self.with_logging
+        if log_level is None:
+            log_level = self.log_level
         if channel not in self.subscribers:
             self.subscribers[channel] = []
         self.subscribers[channel].append(Subscriber(subscriber, handler))
         if with_logging:
-            print(f"Added subscriber to {channel} channel")
+            if isinstance(with_logging, Logger):
+                with_logging.log(log_level, f"Added subscriber to {channel} channel")
+            else:
+                print(f"Added subscriber to {channel} channel")
 
-    def unsubscribe(self, channel, subscriber, with_logging=None):
+    def unsubscribe(self, channel, subscriber, with_logging=None, log_level=None):
         if with_logging is None:
             with_logging = self.with_logging
+        if log_level is None:
+            log_level = self.log_level
         if channel in self.subscribers:
             self.subscribers[channel] = [
                 s for s in self.subscribers[channel] if s.subscriber != subscriber
             ]
         if with_logging:
-            print(f"Removed subscriber from {channel} channel")
+            if isinstance(with_logging, Logger):
+                with_logging.log(log_level, f"Removed subscriber from {channel} channel")
+            else:
+                print(f"Removed subscriber from {channel} channel")
 
-    def close_channel(self, channel, with_logging=None):
+    def close_channel(self, channel, with_logging=None, log_level=None):
         if with_logging is None:
             with_logging = self.with_logging
+        if log_level is None:
+            log_level = self.log_level
         subscribers_count = len(self.subscribers[channel])
         del self.subscribers[channel]
         if with_logging:
-            print(f"{channel} closed ({subscribers_count} subscribers)")
+            if isinstance(with_logging, Logger):
+                with_logging.log(log_level, f"{channel} closed ({subscribers_count} subscribers)")
+            else:
+                print(f"{channel} closed ({subscribers_count} subscribers)")
