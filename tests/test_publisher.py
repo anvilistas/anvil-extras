8c4a0673a0c8704d03de61eacfd4a8953cad6b96
Duro <gabriel.duro@sonner.com.br>
2024-11-03 08:55:19 -0300

    Fixed test_publisher.py for logging.

--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -2,7 +2,8 @@ from client_code import messaging
 
 
 def test_default_logging(capsys):
-    publisher = messaging.Publisher()
+    from client_code.logging import Logger, DEBUG
+    publisher = messaging.Publisher(logger=Logger(level=DEBUG))
     publisher.publish("test_channel", "test_message")
     captured = capsys.readouterr()
     assert (
@@ -12,24 +13,7 @@ def test_default_logging(capsys):
 
 
 def test_no_logging_default(capsys):
-    publisher = messaging.Publisher(with_logging=False)
+    publisher = messaging.Publisher()
     publisher.publish("test_channel", "test_message")
     captured = capsys.readouterr()
     assert captured.out == ""
-
-
-def test_default_logging_override(capsys):
-    publisher = messaging.Publisher()
-    publisher.publish("test_channel", "test_message", with_logging=False)
-    captured = capsys.readouterr()
-    assert captured.out == ""
-
-
-def test_no_logging_override(capsys):
-    publisher = messaging.Publisher(with_logging=False)
-    publisher.publish("test_channel", "test_message", with_logging=True)
-    captured = capsys.readouterr()
-    assert (
-        captured.out
-        == "Published 'test_message' message on 'test_channel' channel to 0 subscriber(s)\n"
-    )
