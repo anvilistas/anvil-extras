c75dc2ebb231758312ab126c732cc68a2a9a8886
stu <stu@anvil.works>
2025-03-17 22:25:32 +0800

    remove the breaking change

--- client_code/Slider/__init__.py
+++ client_code/Slider/__init__.py
@@ -39,6 +39,9 @@ _html_injector.css(
 .ae-slider-container.has-pips {{
   padding-bottom: 40px;
 }}
+.anvil-container-overflow, .anvil-panel-col {{
+    overflow: visible;
+}}
 .noUi-connect {{
   background: var({BAR_COLOR});
 }}
