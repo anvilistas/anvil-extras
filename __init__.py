ff5299902aa1cfa7aa8cc41cf95ab21dfbd14b57
stu <stu@anvil.works>
2025-08-12 23:32:10 +0800

    refactor: use os.path for more reliable path handling in __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,9 @@
 # pip install anvil-uplink
 # python -m anvil.run_app_via_uplink YourAppPackageName
 
-__path__ = [__path__[0] + "/server_code", __path__[0] + "/client_code"]
+import os
+
+__path__ = [
+    os.path.join(os.path.dirname(__file__), "server_code"),
+    os.path.join(os.path.dirname(__file__), "client_code"),
+]
